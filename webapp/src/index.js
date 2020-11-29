3c1fc3cbadb9a5408c1121347001fc1a3d8fdf93
Davud Gayibov <36672821+ZigZag48@users.noreply.github.com>
2020-11-29 12:37:38 +0100

    Update index.js

--- webapp/src/index.js
+++ webapp/src/index.js
@@ -6,10 +6,10 @@ import { router } from './_helper';
 import App from './app/App.vue';
 
 // setup fake backend
-import { configureFakeBackend } from './_helper';
+//import { configureFakeBackend } from './_helper';
 
 Vue.use(VeeValidate);
-configureFakeBackend();
+//configureFakeBackend();
 
 // eslint-disable-next-line no-new
 new Vue({
